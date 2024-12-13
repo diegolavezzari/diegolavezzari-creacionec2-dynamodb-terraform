1fbe6ee078cd24ed657200fa1a8773705058eec1
Diego <diego.lavezzari@gmail.com>
2024-12-12 22:13:29 -0300

    bucket mundose22

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mundose2024"
+    bucket         = "mundose22"
     region         = "us-east-1"
     key            = "backend.tfstate"
     dynamodb_table = "terraform-state-lock"
