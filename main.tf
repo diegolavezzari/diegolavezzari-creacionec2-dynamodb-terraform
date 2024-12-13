9cb428220214f96b913e1606e03bc27a63913bd9
Diego <diego.lavezzari@gmail.com>
2024-12-12 22:22:07 -0300

    se arregla main

--- main.tf
+++ main.tf
@@ -9,6 +9,15 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
+resource "aws_dynamodb_table" "terraform_lock_table" {
+  name           = "terraform-lock-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0453ec754f44f9a4a"
@@ -17,15 +26,5 @@ resource "aws_instance" "app_server" {
 
   tags = {
     Name = var.instance_name
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_lock_table" {
-  name           = "terraform-lock-table"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
   }
 }
