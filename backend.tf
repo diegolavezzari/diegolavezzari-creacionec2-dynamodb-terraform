5d27787faf0ef79f21130e22be051fb1c67218a1
Diego <diego.lavezzari@gmail.com>
2024-12-16 22:51:39 -0300

    backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = var.aws_s3_bucket
-    region         = var.region
+    bucket         = "terraform-backup"
+    region         = "us-east-1"
     key            = "backend.tfstate"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
